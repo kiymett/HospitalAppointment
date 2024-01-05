2104fc24c40d2234c25360707e4ddc43ef1c9c5a
kiymettt <leylakkdemir@gmail.com>
2024-01-05 22:44:21 +0100

    deleted comment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,20 +9,23 @@ const Home = () => {
   const handleAdd = (newAppointment) => {
     setAppointments([...appointments, newAppointment])
   }
-
+  
   const handleDelete = (id) => {
     const filteredList = appointments.filter((item) => item.id !== id)
     setAppointments(filteredList);
+   
+
   }
 
   const handleDoubleClick= (id) => {
     console.log(id)
     const updatedList = appointments.map(item => item.id === id ? {...item, consulted: !item.consulted}: item);
     setAppointments(updatedList);
+  
   } 
   return (
     <main className="text-center mt-2">
-      <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
+      <h1 className="display-5 text-danger">KRYSTAL HOSPITAL</h1>
       <Doctors handleAdd = {handleAdd}/>
       <AppointmentList appointments = {appointments} 
       handleDelete = {handleDelete} 
